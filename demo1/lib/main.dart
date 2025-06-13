945d199a132d8431f927a2a761edc016255e2e2c
Santhoji07 <santhoji07msd@gmail.com>
2025-06-13 13:06:43 +0530

    Initial commit

---
+++ demo1/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hello Chat",
+              style: GoogleFonts.lato(
+                textStyle: TextStyle(color: Colors.blue, letterSpacing: .5),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.deepOrange,
+    );
+  }
+}
